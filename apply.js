d79841e051fe135aafc0e01019dff8a5aea01932
Jake Coble <jakecoble@users.noreply.github.com>
2018-01-05 21:22:10 -0800

    set up webpage loading

--- apply.js
+++ apply.js
@@ -1,6 +1,24 @@
 const figlet = require('figlet');
 const gradient = require('gradient-string');
 
+const Promise = require('bluebird');
+const fs = require('fs');
+const puppeteer = require('puppeteer');
+const parse = Promise.promisify(require('csv-parse'));
+
+const readFile = Promise.promisify(fs.readFile);
+
+function applyToRecord(record) {
+  var url = record[2];
+
+  puppeteer.launch({ headless: false })
+    .then(async browser => {
+      const page = await browser.newPage();
+      await page.goto(url);
+      await browser.close();
+    });
+}
+
 function apply (argv) {
   var banner = figlet.textSync('Indeed Apply', {
     font: 'Poison'
@@ -8,6 +26,10 @@ function apply (argv) {
 
   banner = gradient(['green', 'yellow']).multiline(banner);
   console.log(banner);
+
+  readFile(argv.o || 'jobs.csv')
+    .then(contents => parse(contents, { delimiter: ',' }))
+    .then(records => records.forEach(applyToRecord));
 }
 
 module.exports = {
